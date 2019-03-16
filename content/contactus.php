29d181381cc514226fb85bdd071c2cd357e34a34
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2019-03-16 11:24:27 +0700

    Add files via upload

---
+++ content/contactus.php
@@ -0,0 +1,32 @@
+<section class="contactus" id="contactus">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 leftalign">
+                <h1>Contact Us</h1>
+                <p>Send us a message for an inquiry.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" name="nametext" class="form-control" placeholder="Your Full Name" value="">
+              </div>
+              <div class="form-group">
+                  <input type="text" name="phonenumber" class="form-control" placeholder="Your Phone Number" value="" min="10" max="12" title="Min. Characters is 10, Max. Characters is 12">
+              </div>
+              <div class="form-group">
+                <input type="text" name="textemail" class="form-control" placeholder="Your Email" value="">
+              </div>
+              <div class="form-group">
+                <textarea name="msg" class="form-control" cols="30" rows="10" placeholder="Your Message"></textarea>
+              </div>
+              <div class="form-group">
+                <input type="submit" name="submitbtn" class="submitbtn" value="Send Message">
+              </div>
+            </div>
+            <div class="col-md-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.930663780247!2d98.70931801495672!3d3.6033515973778893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303131f1d1dbbd6f%3A0xf6124cb1cbd2f412!2sBird+Nest%2C+PT.+ORI+GINALNEST+INDONESIA!5e0!3m2!1sid!2sid!4v1552653728348" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ content/gallery_home.php
@@ -0,0 +1,13 @@
+<section class="ourGallery" id="ourGallery">
+    <div class="row">
+        <div class="container">
+            <div class="col-md popup-gallery">
+                <a href="/img/ori01.jpg"><img src="/img/ori01.jpg" alt="Original Product Nest 01"></a>
+                <img src="/img/ori02.jpg" alt="Original Product Nest 01">
+                <img src="/img/ori03.jpg" alt="Original Product Nest 01">
+                <img src="/img/ori04.jpg" alt="Original Product Nest 01">
+                <img src="/img/ori05.jpg" alt="Original Product Nest 01">
+            </div>
+        </div>
+    </div>
+</section>
